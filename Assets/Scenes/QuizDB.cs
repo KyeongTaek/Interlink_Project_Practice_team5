5fba67febd223814ac47474b8ef86d6d9d581b66
doseong :) <118369702+HappyJs-spring@users.noreply.github.com>
2025-11-21 00:50:00 +0900

    Quiz

---
+++ Assets/Scenes/QuizDB.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine;
+
+public class QuizDB
+{
+    private string GetDBPath()
+    {
+        return "URI=file:" + Application.streamingAssetsPath + "/Quiz.db";
+    }
+
+    // 문제 리스트 불러오기
+    public List<string> LoadQuestions()
+    {
+        List<string> questions = new List<string>();
+
+        using (IDbConnection conn = new SqliteConnection(GetDBPath()))
+        {
+            conn.Open();
+            using (IDbCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT question FROM Quiz";
+
+                using (IDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string q = reader.GetString(0);
+                        questions.Add(q);
+                    }
+                }
+            }
+        }
+
+        return questions;
+    }
+}
