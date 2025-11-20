2707dcd8a64453f93549fae90b092cb9738a7295
KyeongTaek <tommyholic@naver.com>
2025-11-21 06:19:43 +0900

    Add submission saving feature
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- Assets/Scenes/QuizDB.cs
+++ Assets/Scenes/QuizDB.cs
@@ -12,9 +12,9 @@ public class QuizDB
     }
 
     // 문제 리스트 불러오기
-    public List<string> LoadQuestions()
+    public List<KeyValuePair<int, string>> LoadQuestions()
     {
-        List<string> questions = new List<string>();
+        List<KeyValuePair<int, string>> questions = new List<KeyValuePair<int, string>>();
 
         using (IDbConnection conn = new SqliteConnection(GetDBPath()))
         {
@@ -22,13 +22,15 @@ public class QuizDB
             using (IDbCommand cmd = conn.CreateCommand())
             {
                 //cmd.CommandText = "SELECT question FROM Quiz";
-                cmd.CommandText = "SELECT content FROM Question";
+                cmd.CommandText = "SELECT question_id, content FROM Question";
                 using (IDataReader reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        string q = reader.GetString(0);
-                        questions.Add(q);
+                        int id = reader.GetInt32(0);
+                        string q = reader.GetString(1);
+                        KeyValuePair<int, string> pair = new KeyValuePair<int, string>(id, q);
+                        questions.Add(pair);
                     }
                 }
             }
--- Assets/Scenes/QuizManager.cs
+++ Assets/Scenes/QuizManager.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using System.Data;
+using Mono.Data.Sqlite;
 
 public class QuizManager : MonoBehaviour
 {
     public TMP_Text questionText;
 
-    private List<string> questions;
+    private List<KeyValuePair<int, string>> questions;
     private int currentIndex = 0;  // 현재 문제 번호
+
+    private char submitAnswer;
 
     void Start()
     {
@@ -50,9 +54,69 @@ public class QuizManager : MonoBehaviour
         }
     }
 
-    private void ShowQuestion(int index)
+    public void ShowQuestion(int index)
     {
-        questionText.text = questions[index];
-        Debug.Log("현재 문제: " + questions[index]);
+        questionText.text = questions[index].Value;
+        Debug.Log("현재 문제: " + questionText.text);
+
+        // 응답 결과가 나왔다고 가정
+        submitAnswer = 'O';
+        KeyValuePair<int, string> temp = questions[currentIndex];
+        bool rst = CheckAnswer(temp.Key, submitAnswer); // 정답과 비교
+        InsertLog(rst, temp.Key); // 기록 저장
+    }
+
+    // 응답 결과와 정답 비교하는 함수
+    public bool CheckAnswer(int curIdx, char submitAnswer)
+    {
+        bool rst = false;
+
+        using (IDbConnection conn = new SqliteConnection("URI=file:" + Application.streamingAssetsPath + "/test.db"))
+        {
+            conn.Open();
+            using (IDbCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT answer FROM Question where question_id = " + curIdx;
+                using (IDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string answer = reader.GetString(0);
+                        if (answer[0] == submitAnswer)
+                        {
+                            rst = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return rst;
+    }
+
+    // 기록을 db에 저장하는 함수
+    public void InsertLog(bool rst, int q_id)
+    {
+        using (IDbConnection conn = new SqliteConnection("URI=file:" + Application.streamingAssetsPath + "/test.db"))
+        {
+            conn.Open();
+            using (IDbCommand cmd = conn.CreateCommand())
+            {
+                string is_correct = "";
+                if(rst)
+                {
+                    is_correct = "Y";
+                }
+                else
+                {
+                    is_correct = "N";
+                }
+                cmd.CommandText = "INSERT INTO Log VALUES(" + PlayerPrefs.GetInt("SessionId") + ", " + q_id + ", " + is_correct + ")";
+                //cmd.ExecuteNonQuery();
+                Debug.Log("sql: " + cmd.CommandText);
+                cmd.Dispose();
+            }
+            conn.Close();
+        }
     }
 }
