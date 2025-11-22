a6597538c4f32e2c94f9ade26625a7538cf810c5
YangHeeChang <diogmlckd@naver.com>
2025-11-22 23:46:40 +0900

    update

--- Assets/Video/End.cs
+++ Assets/Video/End.cs
@@ -1,17 +1,36 @@
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.SceneManagement; // 씬 이동을 위해 필수
 
 public class End : MonoBehaviour
 {
-    public GameObject videoPanel;      // 영상 패널
-    public VideoPlayer videoPlayer;    // VideoPlayer 컴포넌트
+    public GameObject videoPanel;
+    public VideoPlayer videoPlayer;
+
+    public string nextSceneName = "Game";
+    public int currentStageNumber = 1;
 
     void Start()
     {
-        // 영상 패널 켜기
         videoPanel.SetActive(true);
-
-        // 영상 재생
         videoPlayer.Play();
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        SaveClearData();
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    void SaveClearData()
+    {
+        int nextStage = currentStageNumber + 1;
+        if (nextStage > PlayerPrefs.GetInt("ClearedLevel", 1))
+        {
+            PlayerPrefs.SetInt("ClearedLevel", nextStage);
+            PlayerPrefs.Save();
+            Debug.Log("스테이지 클리어 저장 완료: " + nextStage);
+        }
     }
 }
